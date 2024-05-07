b3b775cbc49a49cca3fe0064e3ad98a446bf09c5
Olexa-Klyus <olexakljus@gmail.com>
2024-05-07 20:33:07 +0300

    part 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
-import React, {FC} from 'react';
+import React from 'react';
 import './App.css';
+import Caracter from "./components/character/Character"
 
-const App: FC = () => {
+const App = () => {
     return (
         <div>
+            <Caracter
+                name={'bart'}
+                img={'https://upload.wikimedia.org/wikipedia/en/a/aa/Bart_Simpson_200px.png'}
+            >
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur, illo?
 
+                </Caracter>
+            <Caracter
+                name={'homer'} img={'https://upload.wikimedia.org/wikipedia/en/0/02/Homer_Simpson_2006.png'}/>
         </div>
     );
 }
