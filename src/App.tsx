97cb8d47b52aa880a8c8bb940261b617bff8be08
Olexa-Klyus <olexakljus@gmail.com>
2024-05-08 21:25:50 +0300

    add characters

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,11 @@
 import React from 'react';
 import './App.css';
-import Caracter from "./components/character/Character"
+import Caracters from "./components/characters/Characters"
 
 const App = () => {
     return (
         <div>
-            <Caracter
-                name={'bart'}
-                img={'https://upload.wikimedia.org/wikipedia/en/a/aa/Bart_Simpson_200px.png'}
-            >
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur, illo?
-
-                </Caracter>
-            <Caracter
-                name={'homer'} img={'https://upload.wikimedia.org/wikipedia/en/0/02/Homer_Simpson_2006.png'}/>
+            <Caracters/>
         </div>
     );
 }
