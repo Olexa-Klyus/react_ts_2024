36485e3a4270b7e2b08251c9e0a91696c2a4be1a
Olexa-Klyus <olexakljus@gmail.com>
2024-05-09 20:09:37 +0300

    hw1

---
+++ src/components/products/Producrs.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Producrs = () => {
+
+    // https://dummyapi.online/api/products
+
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default Producrs;
