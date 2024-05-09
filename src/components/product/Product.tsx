e9e682458aa1b435ea32629a7c57eae714bc079d
Olexa-Klyus <olexakljus@gmail.com>
2024-05-09 20:15:08 +0300

    hw1

--- src/components/product/Product.tsx
+++ src/components/product/Product.tsx
@@ -1,6 +1,19 @@
-import React from 'react';
+import React, {FC} from 'react';
 
-const Product = () => {
+interface IProductProps = {
+    "id": ,
+    "title": "iPhone 9",
+    "description": "An apple mobile which is nothing like apple",
+    "price": 549,
+    "discountPercentage": 12.96,
+    "rating": 4.69,
+    "stock": 94,
+    "brand": "Apple",
+    "category": "smartphones",
+    "thumbnail": "https://cdn.dummyjson.com/product-images/1/thumbnail.jpg",
+    "images": [
+}
+const Product:FC<IProductProps> = () => {
     return (
         <div>
             
--- src/components/products/Producrs.tsx
+++ src/components/products/Producrs.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Producrs = () => {
 
-    // https://dummyapi.online/api/products
+    //https://dummyjson.com/products
 
     return (
         <div>
