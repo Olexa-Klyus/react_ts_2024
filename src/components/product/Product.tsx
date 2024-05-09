0dad56a8fdca203c49bbc08fad30aed85a3adbdb
Olexa-Klyus <olexakljus@gmail.com>
2024-05-09 20:18:35 +0300

    hw1

--- src/components/product/Product.tsx
+++ src/components/product/Product.tsx
@@ -1,22 +1,23 @@
 import React, {FC} from 'react';
 
-interface IProductProps = {
-    "id": ,
-    "title": "iPhone 9",
-    "description": "An apple mobile which is nothing like apple",
-    "price": 549,
-    "discountPercentage": 12.96,
-    "rating": 4.69,
-    "stock": 94,
-    "brand": "Apple",
-    "category": "smartphones",
-    "thumbnail": "https://cdn.dummyjson.com/product-images/1/thumbnail.jpg",
-    "images": [
+interface IProductProps {
+    "id": number,
+    "title": string,
+    "description": string,
+    "price": number,
+    "discountPercentage": number,
+    "rating": number,
+    "stock": number,
+    "brand": string,
+    "category": string,
+    "thumbnail": string,
+    "images": string[]
 }
-const Product:FC<IProductProps> = () => {
+
+const Product: FC<IProductProps> = () => {
     return (
         <div>
-            
+            <h2>{}</h2>
         </div>
     );
 };
