7856868c740f9d9b2e63b34f7c15a11dc599e513
Olexa-Klyus <olexakljus@gmail.com>
2024-05-09 20:43:39 +0300

    hw1

--- src/components/products/Producrs.tsx
+++ src/components/products/Producrs.tsx
@@ -1,11 +1,18 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 
 const Producrs = () => {
 
 
-    const [products, setProducts] = useState()
+    const [products, setProducts] = useState([])
     //https://dummyjson.com/products
 
+    useEffect(() => {
+        fetch("https://dummyjson.com/products")
+            .then(value => value.json())
+            .then(value => {
+                setProducts(value)
+            })
+    }, []);
     return (
         <div>
 
