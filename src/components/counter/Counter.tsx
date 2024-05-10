6cba89847f23247e7e831c4e8b3d53608db92030
Olexa-Klyus <olexakljus@gmail.com>
2024-05-10 14:46:48 +0300

    hw 1 +counter css

--- src/components/counter/Counter.tsx
+++ src/components/counter/Counter.tsx
@@ -1,9 +1,19 @@
 import React, {useEffect, useState} from "react";
+
+
+
 import classes from "./Counter.module.css";
 
 export const Counter = () => {
 
     const [count, setCount] = useState(0);
+    const [posts, setPosts] = useState([])
+
+    useEffect(() => {
+        fetch("https://jsonplaceholder.typicode.com/posts")
+            .then((value) => value.json())
+            .then((value) => console.log(value));
+    }, []);
 
     useEffect(() => {
         console.log('count has been changed')
@@ -14,9 +24,16 @@ export const Counter = () => {
 
     return (
         <div>
-            <div className={classes.wrapper}>Counter - {count ? count : 'no counter'}
+            <div className={classes.wrapper}>
+                Counter - {count ? count : 'no count'}
                 <button className={classes.button} onClick={handleInc}>Increment</button>
                 <button className={classes.button} onClick={handleDec}>Decrement</button>
+
+                {posts.map((post:any) => (
+                    <div key={post.id}>
+                        <h3>{post.title}</h3>
+                    </div>
+                ))}
             </div>
         </div>
     )
