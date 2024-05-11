b5e567c01f0759349d46ec3b2fbebd6239f5ef2f
Olexa-Klyus <olexakljus@gmail.com>
2024-05-11 13:10:57 +0300

    les2- lifting basic

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
 import React, {FC} from 'react';
 import './App.css';
+import UsersComponent from "./components/UsersComponent";
 
 const App: FC = () => {
+
+    const lift=(user:any)=>{
+        console.log(user);
+    }
     return (
         <div>
+            <UsersComponent lift={lift}/>
+            <hr/>
 
+
+
+            <hr/>
         </div>
     );
 }
---
+++ src/components/UsersComponent.tsx
@@ -0,0 +1,21 @@
+import React, {FC} from 'react';
+
+let users = [
+    {name: 'Vasya', age: 31, status: false},
+    {name: 'Petya', age: 25, status: true},
+    {name: 'Kolya', age: 45, status: true},
+    {name: 'Anya', age: 28, status: false},
+]
+
+const UsersComponent:FC<any> = ({lift}) => {
+
+    return (
+        <div style={{border: '1px solid', background: 'silver'}}>
+            {users.map((user, index) => (<div key={index}>{user.name}
+                <button onClick={()=>{lift(user)}}>User</button>
+            </div>))}
+        </div>
+    );
+};
+
+export default UsersComponent;
