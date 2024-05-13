6500e7649d0fdcc5e61e35eb6a9aba674bc7ee26
Olexa-Klyus <olexakljus@gmail.com>
2024-05-13 12:29:33 +0300

    useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 
 const App = () => {
+
+    const [counter, setCounter] = useState<number>(0)
+    const [x, setX] = useState(0)
+
+    useEffect(() => {
+
+        setTimeout(() => {
+            setCounter(counter + 1);
+        }, 2000);
+
+    }, [x]);
+
+    console.log('..')
     return (
         <div>
-
+            <h2>{counter}</h2>
+            <button onClick={() => {
+                setX((x) => x + 1);
+            }}>reset
+            </button>
         </div>
     );
 }
