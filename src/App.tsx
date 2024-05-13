63d73c6c0d72fd9cd251f014a755439ce7802871
Olexa-Klyus <olexakljus@gmail.com>
2024-05-13 17:22:38 +0300

    hook useCallback

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
-import React, {memo, useState} from 'react';
+import React, {FC, memo, useCallback, useState} from 'react';
 import './App.css';
 
 // щоб не перерендерювати постійно статичний компонент, потрібно його тіло огорнути в хук memo
 // він перевіряє, якщо всередині memo змін нема, то перерендеру не буде
+// але якщо туди заходить аргументом посилання на функцію, це розцінюється як щось нове
+// і буде рендер. Якщо є складна логіка, яка довго виконується, її треба огорнути
+// в useCallback(), щоб уникнути постійного виконання при рендері
+// і в deps вказати тригер, наприклад counter
 
-const A = memo(() => {
+const A: FC<any> = memo(({foobar}) => {
     console.log('A render');
+    foobar();
     return <div>hello</div>
 })
 
@@ -14,9 +19,14 @@ const App = () => {
 
     const [counter, setCounter] = useState(0)
 
+
+    const foobar = useCallback(() => {
+        //.... складна логіка
+    }, [counter]);
+
     return (
         <div>
-            <A/>
+            <A foobar={foobar}/>
             <button onClick={() => {
                 setCounter(counter + 1);
             }}>click
