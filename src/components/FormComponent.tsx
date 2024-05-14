106b86020515dfc82c55a902dda195344aa69b7d
Olexa-Klyus <olexakljus@gmail.com>
2024-05-14 07:03:07 +0300

    useForm

--- src/components/FormComponent.tsx
+++ src/components/FormComponent.tsx
@@ -1,10 +1,22 @@
 import React, {FC} from 'react';
+import {useForm} from "react-hook-form";
+
+interface IFormProps {
+    username: string,
+    age: number,
+    password: string,
+}
 
 const FormComponent: FC = () => {
+    let form = useForm<IFormProps>();
+    console.log(form);
+
+
     return (
         <div>
             <form>
                 <input type='text' name={'username'}/>
+                <input type='number' name={'age'}/>
                 <input type='text' name={'password'}/>
                 <button>save</button>
             </form>
