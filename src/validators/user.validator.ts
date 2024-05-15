915e7f1c40c6a9089690d9f4640a6c3a42c9de71
Olexa-Klyus <olexakljus@gmail.com>
2024-05-15 09:41:22 +0300

    useFormValidation Joi

--- src/validators/user.validator.ts
+++ src/validators/user.validator.ts
@@ -1,6 +1,7 @@
 import Joi from "joi";
 
 export const userValidator = Joi.object({
+
     username: Joi
         .string()
         .pattern(/w{4,}/)
@@ -10,6 +11,8 @@ export const userValidator = Joi.object({
             "string.pattern.base": "only words accepted",
             "string.required.base": "username is required",
         }),
+    // password: Joi.string(),
+
     age: Joi.number().min(0).max(120).required().messages({
         'number.min': "min 0",
         'number.max': "max 120",
