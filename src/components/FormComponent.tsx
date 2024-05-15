a55b6185b06dee64628471dc3bcc6b6bea6ce450
Olexa-Klyus <olexakljus@gmail.com>
2024-05-15 10:39:23 +0300

    useForm -fetch post

--- src/components/FormComponent.tsx
+++ src/components/FormComponent.tsx
@@ -6,6 +6,7 @@ import {postValidator} from "../validators/post.validator";
 interface IFormProps {
     title: string,
     body: string,
+    userId: number,
 }
 
 const FormComponent: FC = () => {
@@ -22,7 +23,21 @@ const FormComponent: FC = () => {
 
     const
         save = (formValues: IFormProps) => {
-            console.log(formValues) // отримуємо значення з інпутів
+
+
+            fetch('https://jsonplaceholder.typicode.com/posts', {
+                method: 'POST',
+                body: JSON.stringify({
+                    title: formValues.title,
+                    body: formValues.body,
+                    userId: formValues.userId,
+                }),
+                headers: {
+                    'Content-type': 'application/json; charset=UTF-8',
+                },
+            })
+                .then((response) => response.json())
+                .then((json) => console.log(json));
         }
 
     return (
@@ -36,6 +51,11 @@ const FormComponent: FC = () => {
                 <input type='text' {...register('body')}/>
                 {errors.body && <span>{errors.body.message}</span>}
                 <br/>
+
+                <input type='text' {...register('userId')}/>
+                {errors.userId && <span>{errors.userId.message}</span>}
+                <br/>
+
 
                 <button>save</button>
             </form>
--- src/validators/post.validator.ts
+++ src/validators/post.validator.ts
@@ -2,6 +2,13 @@ import Joi from "joi";
 
 export const postValidator = Joi.object({
 
-    title: Joi.string().required().messages({'string.required':'field is required'}),
-    body: Joi.string().required().messages({'string.required':'field is required'})
+    title: Joi.string().required().messages({'string.required': 'field is required'}),
+    body: Joi.string().required().messages({'string.required': 'field is required'}),
+    userId: Joi.number().required().min(1).max(10).messages(
+        {
+            'number.required': 'field is required',
+            'number.min': 'userId is >1',
+            'number.max': 'userId is <11',
+        }
+    )
 })
