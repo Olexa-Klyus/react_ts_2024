631da7a8986b7624542c73d957e67bdcd97eb692
Olexa-Klyus <olexakljus@gmail.com>
2024-05-18 12:03:04 +0300

    consult-Vetal

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,25 +1,20 @@
 import {IUserModel} from "../models/IUserModel";
-import { axiosInstance} from "./api.service";
-import {IResponce} from "../types/responceType";
-import {urls} from "../constants/urls";
-import axios from "axios";
+import axios, {AxiosResponse} from "axios";
 
+let axiosInstance = axios.create({
+    baseURL: 'https://jsonplaceholder.typicode.com',
+    headers: {}
+});
 
-// const userApiService = {
-//     getAllUsers: (): IResponce<IUserModel[]> => {
-//         return (axiosInstance.get(urls.users))
-//     }
-// }
-
-const apiService=axios.create({baseURL: 'https://jsonplaceholder.typicode.com/'});
 const userApiService = {
-    getAllUsers: (): IResponce<IUserModel[]> => {
-        return (axiosInstance.get(urls.users))
-    }
+    getAllUsers: (): Promise<AxiosResponse<IUserModel[]>> => {
+        return axiosInstance.get("/users");
+    },
+    //
+    // getUserByUserId: async (userId: number): Promise<AxiosResponse<IUserModel>> => {
+    //     return await axiosInstance.get(`/users/${userId}`);
+    // },
+
 }
 
-
-
-export {
-    userApiService
-}
+export {userApiService}
