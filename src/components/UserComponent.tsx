ab2371f19405ef66bf14a53bec51632fd4c14a0d
Olexa-Klyus <olexakljus@gmail.com>
2024-05-18 18:30:23 +0300

    routes-useLocation , useNavigate

--- src/components/UserComponent.tsx
+++ src/components/UserComponent.tsx
@@ -1,18 +1,26 @@
 import React, {FC} from 'react';
 import {IUserModel} from "../models/IUserModel";
-import {NavLink} from "react-router-dom";
+import {NavLink, useNavigate} from "react-router-dom";
 
 interface IProps {
     user: IUserModel;
 }
 
+// в state можна передати обєкт
+// а в дочірньому відхопити за допомогою useLocation();
+// або створити callback функцію useNavigate і в неї це все вкласти
+
 const UserComponent: FC<IProps> = ({user}) => {
+    const navigate = useNavigate()
+
     return (
         <div>
             <div>
-                // в state можна передати обєкт
-                // а в дочірньому відхопити за допомогою useLocation();
                 <NavLink to={user.id.toString()} state={{user}}> {user.name}</NavLink>
+
+                <button onClick={() => navigate(user.id.toString(), {state: {user}})}>
+                    show posts
+                </button>
             </div>
         </div>
     );
