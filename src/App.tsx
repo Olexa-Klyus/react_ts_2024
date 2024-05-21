23cc1e40f28986c6641d11a75c012e995f567917
Olexa-Klyus <olexakljus@gmail.com>
2024-05-21 08:58:59 +0300

    useRef

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 
-
+// якщо useState фіксує стан і рендерить, то useRef фіксує стан без рендеру
 const App = () => {
     return (
         <div>
+
+
 
         </div>
     );
