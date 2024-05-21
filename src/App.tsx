2b673bf4312b8a473475a7d87d01726413018000
Olexa-Klyus <olexakljus@gmail.com>
2024-05-21 15:49:35 +0300

    useRef

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import './App.css';
 
 // якщо useState фіксує стан і рендерить, то useRef фіксує стан без рендеру
+// тримає посилання на обєкт в памяті і не втрачається при оновленні сторінки
 const App = () => {
+
+    const [x, setX] = useState<number>(0);
+    const y=useRef(0);
+
+    console.log('work')
+
     return (
         <div>
+            <button onClick={() => {
+                setX((x) => x + 1);
+                console.log(x);
+            }}>change X - {x}
+            </button>
 
-
+            <button onClick={() => {
+                y.current++;
+                console.log(y);
+            }}>change Y - {y.current}
+            </button>
 
         </div>
     );
