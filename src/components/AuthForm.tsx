cbb82f53caeecf1230310290e20417d4c646d6f4
Olexa-Klyus <olexakljus@gmail.com>
2024-05-30 08:59:49 +0300

    auth

--- src/components/AuthForm.tsx
+++ src/components/AuthForm.tsx
@@ -1,6 +1,6 @@
 import React, {useState} from 'react';
 import {useForm} from "react-hook-form";
-import {AuthDataModel} from "../models/AuthDataModel";
+import {AuthDataModel} from "../models";
 
 
 const AuthForm = () => {
