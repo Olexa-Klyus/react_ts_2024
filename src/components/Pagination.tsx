4115aa6e24e31ae41daf286184c76268b1058265
olexa <olexakljus@gmail.com>
2024-06-01 20:38:51 +0300

    pagination

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, {FC} from 'react';
+import {IPaginatedPageModel} from "../models";
 
-const Pagination = () => {
+interface IProps {
+    changePage: (action: string) => void,
+    prev: IPaginatedPageModel | null,
+    next: IPaginatedPageModel | null
+}
+
+const Pagination: FC<IProps> = ({changePage, prev, next}) => {
     return (
         <div>
-            <button>prev</button>
-            <button>next</button>
+            <button onClick={() => {
+                changePage('prev');
+            }}
+                    disabled={!prev}
+            >prev
+            </button>
+
+            <button onClick={() => {
+                changePage('next');
+            }}
+                    disabled={!next}
+            >next
+            </button>
         </div>
     );
 };
