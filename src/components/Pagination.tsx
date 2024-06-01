06eb69a907eb1f6a9c4ab5f03874b29b65cf6b2c
olexa <olexakljus@gmail.com>
2024-06-01 20:28:11 +0300

    pagination

---
+++ src/components/Pagination.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Pagination = () => {
+    return (
+        <div>
+            <button>prev</button>
+            <button>next</button>
+        </div>
+    );
+};
+
+export default Pagination;
