04d7ef576a720337c7260c488e0b86f52f3d0526
olexa <olexakljus@gmail.com>
2024-06-01 18:59:32 +0300

    auth

--- src/components/Car.tsx
+++ src/components/Car.tsx
@@ -5,12 +5,14 @@ const Car = (car: ICarWithAuthModel) => {
     const {id, brand, year, price, photo} = car;
     return (
         <div>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
+            <div>id : {id}</div>
+            <div>brand : {brand}</div>
+            <div>year : {year}</div>
+            <div>price : {price}</div>
+            <div>photo : {photo}</div>
         </div>
     );
 };
 
 export default Car;
+
