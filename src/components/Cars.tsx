0bdd7a08ca4cacd9857a4084686a8acdc4f379e6
olexa <olexakljus@gmail.com>
2024-06-01 18:38:04 +0300

    auth

--- src/components/Cars.tsx
+++ src/components/Cars.tsx
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, {FC, useEffect, useState} from 'react';
+import {carService} from "../services";
+import {ICarWithAuthModel} from "../models";
+import Car from "./Car";
 
 const Cars = () => {
+    const [cars, setCars] = useState<ICarWithAuthModel[]>([])
+    useEffect(() => {
+        carService.getCars().then(({value}) => setCars(value));
+    }, []);
     return (
         <div>
-            
+            {cars.map(car => <Car key={car.id} car={car}/>)}
         </div>
     );
 };
+
 
 export default Cars;
--- src/pages/CarsPage.tsx
+++ src/pages/CarsPage.tsx
@@ -3,14 +3,10 @@ import {carService} from "../services";
 import Cars from "../components/Cars";
 
 const CarsPage = () => {
-    const [cars, setCars] = useState()
-    useEffect(() => {
-        carService.getCars().then(value => setCars(value));
-    }, []);
 
     return (
         <div>
-            <Cars cars={cars}/>
+            <Cars/>
         </div>
     );
 };
