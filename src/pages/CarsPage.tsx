5044b7be4358eb0741faee2305375b23465c069a
olexa <olexakljus@gmail.com>
2024-06-02 11:40:02 +0300

    pagination autorefresh redirect to auth

--- src/pages/CarsPage.tsx
+++ src/pages/CarsPage.tsx
@@ -1,9 +1,10 @@
 import React, {useEffect, useState} from 'react';
-import {carService} from "../services";
+import {authService, carService} from "../services";
 import {ICarPaginatedModel} from "../models";
 import Cars from "../components/Cars";
 import Pagination from "../components/Pagination";
-import {useSearchParams} from "react-router-dom";
+import {useNavigate, useSearchParams} from "react-router-dom";
+import {AxiosError} from "axios";
 
 
 const CarsPage = () => {
@@ -15,19 +16,41 @@ const CarsPage = () => {
         total_items: 0
     })
 
+    const navigate = useNavigate();
+
 // тут тільки зчитуємо значення сторінки, яке впроваджується в компоненті Pagination
     const [query, setQuery] = useSearchParams();
     // console.log('page = ', query.get('page'))
 
+    // виносимо обробку помилок сюди з сервісів, щоб мати можливість робити редіректи, авторефреші і т.д.
+    // для цього потрібно  зробити ще одину локальну async функцію всередині useEffect
+    // в якій викликаємо наші сервіс функції, і вже тут відловлюємо помилки
     useEffect(() => {
-        carService.getCars(query.get('page') || '1').then((data) => {
-            if (data) {
-                setCarsPaginatedObject(data);
+        const getCarsDAta = async () => {
+            try {
+                const response = await carService.getCars(query.get('page') || '1');
+                if (response) {
+                    setCarsPaginatedObject(response);
+                }
+            } catch (e) {
+                const err = e as AxiosError;
+                if (err && err?.response?.status === 401) {
+                    try {
+                        await authService.refresh();
+                    } catch (e) {
+                        return navigate('/');
+                    }
+                    const response = await carService.getCars(query.get('page') || '1');
+                    if (response) {
+                        setCarsPaginatedObject(response);
+                    }
+                }
             }
-        })
+        }
+        getCarsDAta();
     }, [query]);
 
-   return (
+    return (
         <div>
             <Cars cars={carsPaginatedObject?.items}/>
             <Pagination prev={carsPaginatedObject.prev} next={carsPaginatedObject.next}/>
--- src/services/apiService.ts
+++ src/services/apiService.ts
@@ -14,46 +14,29 @@ axiosInstance.interceptors.request.use(request => {
         const iTokenObtainPair = retriveLocalStorageData<ITokenObtainPair>('tokenPair');
         request.headers.set('Authorization', 'Bearer ' + iTokenObtainPair.access)
     }
-
     return request;
 })
 
+// виносимо обробку помилок звідси в CarsPage, щоб мати можливість робити редіректи, авторефреші і т.д.
 const authService = {
     authentication: async (authData: AuthDataModel): Promise<boolean> => {
-        let response;
-        try {
-            response = await axiosInstance.post<ITokenObtainPair>('/auth', authData);
-            localStorage.setItem('tokenPair', JSON.stringify(response.data));
-
-        } catch (e) {
-            console.log(e);
-        }
+        const response = await axiosInstance.post<ITokenObtainPair>('/auth', authData);
+        localStorage.setItem('tokenPair', JSON.stringify(response.data));
         // user аутифікований, якщо в нього є access і refresh
         return !!(response?.data?.access && response?.data?.refresh);
     },
-    // записуємо оновлену пару токенів
-    refresh: async (refreshToken: string) => {
+    // записуємо оновлену пару токенів, але refreshToken беремо з localStorage
+    refresh: async () => {
+        const refreshToken = retriveLocalStorageData<ITokenObtainPair>('tokenPair').refresh;
         const response = await axiosInstance.post<ITokenObtainPair>('/auth/refresh', {refresh: refreshToken});
         localStorage.setItem('tokenPair', JSON.stringify(response.data))
-    },
+    }
 }
 
 const carService = {
-    getCars: async (page: string) => {
-        try {
-            const response = await axiosInstance.get<ICarPaginatedModel>('/cars', {params: {page: page}});
-            return response.data
-        } catch
-            (e) {
-            const axiosError = e as AxiosError;
-            if (axiosError?.response?.status === 401) {
-                //закінчився токен - пробуємо оновити access token через refresh
-                const refreshToken = retriveLocalStorageData<ITokenObtainPair>('tokenPair').refresh;
-                await authService.refresh(refreshToken);
-                await carService.getCars(page);
-            }
-            console.log(e)
-        }
+    getCars: async (page: string = '1') => {
+        const response = await axiosInstance.get<ICarPaginatedModel>('/cars', {params: {page: page}});
+        return response.data
     }
 }
 
