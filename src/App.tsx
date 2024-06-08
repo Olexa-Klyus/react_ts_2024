74d592aa3d76ec64ee9f7e02fac33056dbf1c033
Olexa-Klyus <olexakljus@gmail.com>
2024-06-08 20:56:40 +0300

    redux preview 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,29 @@
 import React from 'react';
 import './App.css';
+import {decrement, increment, incrementByAmount, useAppDispatch, useAppSelector} from "./redux/store";
 
 
 const App = () => {
+
+    const dispatch = useAppDispatch();
+    const counter1State = useAppSelector(state => state.counter1SliceState.value)
     return (
         <div>
+            <h2>{counter1State}</h2>
+            <button onClick={() => {
+                dispatch(increment())
+            }}> inc
+            </button>
 
+            <button onClick={() => {
+                dispatch(decrement())
+            }}> dec
+            </button>
+
+            <button onClick={() => {
+                dispatch(incrementByAmount(10))
+            }}> inc of 10
+            </button>
         </div>
     );
 }
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,4 +1,5 @@
 import {configureStore, createSlice, PayloadAction} from "@reduxjs/toolkit";
+import {useDispatch, useSelector} from "react-redux";
 
 type CounterStateType = {
     value: number;
@@ -40,4 +41,22 @@ const store = configureStore({
     reducer: {
         counter1SliceState: counter1Slice.reducer,
     }
-})
+});
+
+// потрібно параметризувати два хуки, які будуть часто використовуватися в компонентах, щоб потім не робити це кожен раз
+// для цього створюємо два параметризовані кастомні хуки, які будемо використовувати замість них
+
+export type AppDispatch = typeof store.dispatch;
+export const useAppDispatch = useDispatch.withTypes<AppDispatch>();
+
+export type RootState = ReturnType<typeof store.getState>;
+export const useAppSelector = useSelector.withTypes<RootState>();
+
+export default store;
+
+// так було у ВЕТАЛЯ
+// const {reducer: counter1Reducer, actions} = counter1Slice;
+//
+// const counter1Actions = {...actions};
+//
+// export {counter1Actions, counter1Reducer}
