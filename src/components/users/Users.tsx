4f0168c590f0d85e412e0403c5b94a0259871f01
Olexa-Klyus <olexakljus@gmail.com>
2024-06-09 21:26:47 +0300

    petition

--- src/components/users/Users.tsx
+++ src/components/users/Users.tsx
@@ -1,84 +1,115 @@
 import React, {FC, useEffect, useState} from 'react';
 import {getAllRecipes} from "../../services/recipes.api.service";
 
-interface IUser{
-    id:number,
-    name:string,
-    date:string
+
+interface IUser {
+    id: number,
+    name: string,
+    date: string
 }
+
 const Users: FC = () => {
 
-    const [page, setPage] = useState(845)
+        const [page, setPage] = useState(845)
 
-    const [users, setUsers] = useState<IUser[]>([])
+        const [users, setUsers] = useState<IUser[]>([])
 
-    const inc = () => {
-        setPage(page + 1)
-        console.log(page)
+
+        const incrementPage = () => {
+            if (page > 0) {
+                setPage(page - 1)
+            }
+            console.log(page)
+        }
+
+        //___________________________________________________________________________
+        const saveInLocalStorage = () => {
+            let usersStr = ''
+            usersStr += users.map(value => (`${value.id}       ${value.name}                      ${value.date}                `))
+            usersStr = usersStr.replaceAll(",", "\n")
+
+            console.log(usersStr)
+
+            localStorage.setItem("users", usersStr)
+            // localStorage.setItem("users", JSON.stringify(users))
+        }
+        //___________________________________________________________________________
+
+        // const [stateForTimer, setStateForTimer] = useState(30000);
+        //
+        // useEffect(() => {
+        //     // повторити з інтервалом 2 секунди
+        //     let timerId = setInterval(() =>
+        //         incrementPage(), 3000)
+        // }, [incrementPage]);
+
+
+        // useEffect(() => {
+        //     // зупинити через кнопку
+        //     const timeout = setTimeout(() => {
+        //         clearInterval(timerId);
+        //         alert('стоп');
+        //     }, stateForTimer);            
+        // }, [stateForTimer, timerId]);
+
+
+//___________________________________________________________________________
+
+        useEffect(() => {
+            const newArr: any = []
+
+            getAllRecipes(page)
+                .then(value => JSON.stringify(value.data))
+                .then(value => value.slice(0, value.indexOf('","pag_html"')))
+                .then(value => value.slice(value.indexOf('<div class=\\"table_cell number\\">')))
+                .then(value => value.replaceAll('&#039;', ""))
+                .then(value => value.replaceAll('.', ""))
+                .then(value => value.replaceAll(/<div class=\\"table_cell number\\">/g, '"id":'))
+                .then(value => value.replaceAll(/<div class=\\"table_cell name\\">/g, ',"name":"'))
+                .then(value => value.replaceAll(/<div class=\\"table_cell date\\">/g, '","date":"'))
+                .then(value => value.replaceAll(/<div>|<\/div>|\\n/g, ""))
+                .then(value => value.replaceAll(/<div class=\\"table_row\\">/g, '"},{'))
+                .then(value => '[{' + value + '"}]')
+                .then(value => JSON.parse(value))
+
+                .then(value => setUsers(newArr.concat(users, value.reverse())))
+
+
+        }, [page]);
+
+
+        // переробив для текстового файла
+
+
+        console.log(users)
+
+
+        return (
+            <div>
+                <button onClick={() => {
+                    incrementPage()
+                }}>click
+                </button>
+
+                <hr/>
+
+                <button onClick={() => {
+                    saveInLocalStorage()
+                }}>record
+                </button>
+
+                <hr/>
+
+                <button onClick={() => {
+                    // setStateForTimer(1000)
+                    // clearInterval(timerId)
+                    alert('стоп');
+                }}>clearTimeout(timeout)
+                </button>
+
+            </div>
+        );
     }
-
-    const record = () => {
-        const fs = require('fs')
-        const writeStream = fs.createWriteStream('file.txt');
-        const pathName = writeStream.path;
-
-        users.forEach(value => writeStream.write(`${value}\n`));
-
-        // the finish event is emitted when all data has been flushed from the stream
-        writeStream.on('finish', () => {
-            console.log(`wrote all the array data to file ${pathName}`);
-        });
-
-        // handle the errors on the write process
-        writeStream.on('error', (err:any) => {
-            console.error(`There is an error writing the file ${pathName} => ${err}`)
-        });
-
-        // close the stream
-        writeStream.end();
-    }
-
-
-    useEffect(() => {
-
-        const newArr: any = []
-
-        getAllRecipes(page)
-            .then(value => JSON.stringify(value.data))
-            .then(value => value.slice(0, value.indexOf('","pag_html"')))
-            .then(value => value.slice(value.indexOf('<div class=\\"table_cell number\\">')))
-            .then(value => value.replaceAll('&#039;', ""))
-            .then(value => value.replaceAll('.', ""))
-            .then(value => value.replaceAll(/<div class=\\"table_cell number\\">/g, '"id":'))
-            .then(value => value.replaceAll(/<div class=\\"table_cell name\\">/g, ',"name":"'))
-            .then(value => value.replaceAll(/<div class=\\"table_cell date\\">/g, '","date":"'))
-            .then(value => value.replaceAll(/<div>|<\/div>|\\n/g, ""))
-            .then(value => value.replaceAll(/<div class=\\"table_row\\">/g, '"},{'))
-            .then(value => '[{' + value + '"}]')
-            .then(value => JSON.parse(value))
-
-            .then(value => setUsers(newArr.concat(users, value)))
-
-
-    }, [page]);
-
-
-    console.log('users', users)
-
-
-    return (
-        <div>
-            <button onClick={() => {
-                inc()
-            }}>click
-            </button>
-            <hr/>
-            <button onClick={() => {
-                record()
-            }}>record
-            </button>
-        </div>
-    );
-};
+;
 
 export default Users;
