4914befe19d0286564ad80bf6030d02bb1605f4b
Olexa-Klyus <olexakljus@gmail.com>
2024-06-10 10:39:15 +0300

    petition збір підписів- парсинг сторінки, replase, формування str, обхід CORS error

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
-import Recipes from "./components/users/Users";
+import Users from "./components/users/Users";
+
+// запуск хрому з параметрами щоб не било помилку CORS error
+// chrome.exe --user-data-dir="C://chrome-dev-disabled-security" --disable-web-security --disable-site-isolation-trials
 
 const App = () => {
     return (
         <div>
-
-            <Recipes/>
+            <Users/>
         </div>
     );
 }
