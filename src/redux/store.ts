05b27c012daa78837a41a60eb457978b2ef55171
Olexa-Klyus <olexakljus@gmail.com>
2024-06-10 11:50:39 +0300

    redux asynk base

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,90 +1,32 @@
-import {configureStore, createSlice, PayloadAction} from "@reduxjs/toolkit";
-import {useDispatch, useSelector} from "react-redux";
+import {IUser} from "../models";
+import {createAsyncThunk, createSlice} from "@reduxjs/toolkit";
+import {userService} from "../services";
 
-type CounterStateType = {
-    value: number;
+type UserSliceType = {
+    users: IUser[]
 }
-
-// сховище store може складатися з багатьох слайсів
-// кожен слайс створюється за допомогою createSlice з опціями
-// параметризація слайсу робиться за рахунок параметризації initialState
-
-const initialState: CounterStateType = {
-    value: 0,
-};
-
-const initialState2: CounterStateType = {
-    value: 0,
+const userInitState: UserSliceType = {
+    users: []
 }
 
 
-const counter1Slice = createSlice({
-    name: 'counter1SliceName',
-    initialState,
-    reducers: {
-        increment: (state) => {
-            state.value = state.value + 1;
-        },
-        decrement: (state) => {
-            state.value = state.value - 1;
-        },
-        // PayloadAction- це спеціальний тип для параметрицзації action
-        incrementByAmount: (state,
-                            action: PayloadAction<number>) => {
-            state.value = state.value + action.payload
-        },
+const loadUsers = createAsyncThunk(
+    'userSlice/loadUsers',
+    async (_, thunkAPI) => {
+        const users = await userService.getAll();
+        return users;
     }
-});
+);
 
-const counter2Slice = createSlice({
-    name: 'counter2SliceName',
-    initialState:initialState2,
-    reducers: {
-        increment: (state) => {
-            state.value = state.value + 1;
-        },
-        decrement: (state) => {
-            state.value = state.value - 1;
-        },
-        // PayloadAction- це спеціальний тип для параметрицзації action
-        incrementByAmount: (state,
-                            action: PayloadAction<number>) => {
-            state.value = state.value + action.payload
-        },
-    }
-});
+createSlice({
+    name: 'userSlice',
+    initialState: userInitState,
+    reducers: {},
 
-// наші редусери під капотом попадають в counter1Slice.actions, їх треба звідти витягнути і експортнути
-
-export const {
-    increment, decrement, incrementByAmount
-} = counter1Slice.actions;
-
-// крім того потрібно зконфігурувати store, описати з яких слайсів він складається
-const store = configureStore({
-    reducer: {
-        counter1SliceState: counter1Slice.reducer,
-        counter2SliceState: counter2Slice.reducer,
-    }
-});
-
-// --------------------------------------------------------------------------------------------------
-// у ВЕТАЛЯ було по іншому
-const {reducer: counter2Reducer, actions} = counter2Slice;
-
-const counter2Actions = {...actions};
-
-export {counter2Actions, counter2Reducer}
-// --------------------------------------------------------------------------------------------------
-
-// потрібно параметризувати два хуки, які будуть часто використовуватися в компонентах, щоб потім не робити це кожен раз
-// для цього створюємо два параметризовані кастомні хуки, які будемо використовувати замість них
-
-export type AppDispatch = typeof store.dispatch;
-export const useAppDispatch = useDispatch.withTypes<AppDispatch>();
-
-export type RootState = ReturnType<typeof store.getState>;
-export const useAppSelector = useSelector.withTypes<RootState>();
-
-export default store;
+    // всі асинхронні функції описуємо в extraReducers
+    extraReducers: (builder) =>
+        builder.addCase(loadUsers.fulfilled, (state, action) => {
+            state.users = action.payload;
+        })
+})
 
