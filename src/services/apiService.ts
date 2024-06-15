5150391d86eba7129b01e0acf6497cc27ea62d6a
olexa <olexakljus@gmail.com>
2024-06-15 09:54:40 +0300

    redux prev3 routes find by id

--- src/services/apiService.ts
+++ src/services/apiService.ts
@@ -12,7 +12,7 @@ const userService = {
         const response = await axiosInstance.get<IUser[]>(urls.users.base);
         return response.data;
     },
-    getById: async (id: string) => {
+    getById: async (id: string):Promise<IUser> => {
         const response = await axiosInstance.get<IUser>(urls.users.byId(+id));
         return response.data;
     }
